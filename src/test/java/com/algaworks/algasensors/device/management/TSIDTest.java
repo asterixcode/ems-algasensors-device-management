f39e713f0f086cf8db2ae0017af23417b7058253
lucas.saldanha <lucas.saldanha@bestseller.com>
2025-04-07 23:00:40 +0200

    refactor: move IdGenerator package for better organization

--- src/test/java/com/algaworks/algasensors/device/management/TSIDTest.java
+++ src/test/java/com/algaworks/algasensors/device/management/TSIDTest.java
@@ -1,5 +1,6 @@
 package com.algaworks.algasensors.device.management;
 
+import com.algaworks.algasensors.device.management.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
